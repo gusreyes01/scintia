493ff17b4b5581eef6cb424a0136e9b75cd3fb4e
jorge <jtovarm8@gmail.com>
2019-03-29 11:53:44 -0600

    Refresh of Cycle

--- src/app/services/cycle.services.ts
+++ src/app/services/cycle.services.ts
@@ -24,6 +24,7 @@ export class CycleService {
     public cycles2 = [];
     public cycles3 = [];
     public cycle = [];
+    // public protocol;
 
     constructor(
         private storage: Storage,
@@ -60,6 +61,9 @@ export class CycleService {
                 this.storage.remove('cycle_' + protocol.id);
                 this.storage.set('cycle_' + protocol.id, this.cycles2);
                 this.changeProtocol(protocol, this.cycles2);
+                /*protocol.cycles = this.cycles2;
+                this.protocol = protocol;
+                console.log(this.protocol);*/
             } else {
                 this.cycles3 = this.cycles2;
                 this.cycles3.push({
@@ -68,6 +72,9 @@ export class CycleService {
                 this.storage.remove('cycle_' + protocol.id);
                 this.storage.set('cycle_' + protocol.id, this.cycles3);
                 this.changeProtocol(protocol, this.cycles3);
+                /*protocol.cycles = this.cycles3;
+                this.protocol = protocol;
+                console.log(this.protocol);*/
             }
         });
     }
